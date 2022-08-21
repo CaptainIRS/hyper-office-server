de06c748a8919af6d729ca8743d04cbed876fbdc
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2022-08-21 18:06:08 +0530

    fix: Fix IPFS config

--- ipfs/prerequisites.sh
+++ ipfs/prerequisites.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+. ../.env
+
 # Download the WebUI files since our swarm is not connected to the public IPFS network
 mkdir -p webui
 pushd webui
-curl https://ipfs.io/api/v0/get/bafybeibozpulxtpv5nhfa2ue3dcjx23ndh3gwr5vwllk7ptoyfwnfjjr4q | tar -xf -
-wget -O bafybeibozpulxtpv5nhfa2ue3dcjx23ndh3gwr5vwllk7ptoyfwnfjjr4q.dag -c "https://ipfs.io/api/v0/dag/export?arg=bafybeibozpulxtpv5nhfa2ue3dcjx23ndh3gwr5vwllk7ptoyfwnfjjr4q"
+curl $IPFS_WEB_TAR_URL | tar -xf -
+wget -O bafybeibozpulxtpv5nhfa2ue3dcjx23ndh3gwr5vwllk7ptoyfwnfjjr4q.dag -c "$IPFS_WEB_DAG_URL"
 popd

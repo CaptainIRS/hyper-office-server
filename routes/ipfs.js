2b65d182d2bf59e3d37fbf8aa6017de01b91c24c
Anirudh V S <54744932+anirudhvs@users.noreply.github.com>
2022-08-21 16:46:20 +0530

    Added proxy for ipfs files (#5)

---
+++ routes/ipfs.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+var http = require('http');
+
+router.get('/:id', async function(req, res) {
+    // Can add further checks
+    // Can also be used in consent route
+    if(!req.isAuthenticated()) {
+        res.status(401).send('Unauthorized');
+    }
+    try {
+        var externalReq = http.request({
+            hostname: 'localhost',
+            port: '8080',
+            path: `/ipfs/${req.params.id}`,
+        }, function(externalRes) {
+            if (externalRes.headers['content-type'] === 'application/pdf') {
+                res.setHeader('Content-Type', externalRes.headers['content-type']);
+                res.setHeader('Content-Length', externalRes.headers['content-length']);
+                console.log(externalRes.headers['content-type']);
+                externalRes.pipe(res);
+            }
+            else {
+                console.log("Error Fetching file from IPFS");
+                var body = '';
+                externalRes.on('data', function(chunk) {
+                    body += chunk;
+                })
+                externalRes.on('end', function() {
+                    console.log(body);
+                })
+                res.status(500).send({
+                    message: 'Error getting file'
+                });
+            }
+        })
+        externalReq.end();
+    }
+    catch (err) {
+        res.status(500).send({message: 'Error getting file'});
+    }
+});
+
+module.exports = router;

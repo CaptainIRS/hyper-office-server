2ae2d1254960377d25267e738c96db1d76a6716e
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2022-08-22 16:39:55 +0530

    feat: Add API to get blockchain user

--- utils/blockchain.js
+++ utils/blockchain.js
@@ -22,28 +22,28 @@ const stakeholdersCA = CAUtil.buildCAClient(
 );
 
 module.exports.enrollUser = async (email) => {
-  const wallet = await AppUtil.buildWallet(fabricNetwork.Wallets);
+  const wallet = await AppUtil.buildWallet(fabricNetwork.Wallets, '../wallet');
   await CAUtil.enrollAdmin(stakeholdersCA, wallet, "StakeholdersMSP");
   return await CAUtil.registerAndEnrollUser(stakeholdersCA, wallet, "StakeholdersMSP", email, "stakeholders.user");
 }
 
 module.exports.enrollAdministrator = async (email) => {
-  const wallet = await AppUtil.buildWallet(fabricNetwork.Wallets);
+  const wallet = await AppUtil.buildWallet(fabricNetwork.Wallets, '../wallet');
   await CAUtil.enrollAdmin(administrationCA, wallet, "AdministrationMSP");
   return await CAUtil.registerAndEnrollUser(administrationCA, wallet, "AdministrationMSP", email, "administration.administrator");
 }
 
 module.exports.enrollModerator = async (email) => {
-  const wallet = await AppUtil.buildWallet(fabricNetwork.Wallets);
+  const wallet = await AppUtil.buildWallet(fabricNetwork.Wallets, '../wallet');
   await CAUtil.enrollAdmin(administrationCA, wallet, "AdministrationMSP");
   return await CAUtil.registerAndEnrollUser(administrationCA, wallet, "AdministrationMSP", email, "administration.moderator");
 }
 
-const performTransaction = async (user, transactionName, transactionType, args) => {
+const performTransaction = async (user, transactionName, transactionType, ...args) => {
   if (!["Administrator", "Moderator", "User"].includes(user.role)) {
     throw new Error("Invalid user");
   }
-  const wallet = await AppUtil.buildWallet(fabricNetwork.Wallets);
+  const wallet = await AppUtil.buildWallet(fabricNetwork.Wallets, '../wallet');
   if (user.role === "User") {
     await CAUtil.enrollAdmin(stakeholdersCA, wallet, "StakeholdersMSP");
     await CAUtil.registerAndEnrollUser(stakeholdersCA, wallet, "StakeholdersMSP", user.email, "stakeholders.user");
@@ -102,4 +102,9 @@ module.exports.queryFilesOfOwner = async (owner) => {
 
 module.exports.queryFilesOfApprover = async (approver) => {
   return await performTransaction(owner, "queryFilesOfApprover", "evaluate", [approver]);
+}
+
+module.exports.getBlockchainUser = async (email) => {
+  const wallet = await AppUtil.buildWallet(fabricNetwork.Wallets, '../wallet');
+  return await wallet.get(email);
 }
